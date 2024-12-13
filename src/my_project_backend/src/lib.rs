d9dabb12cff9258760e1dd6bb7a2b3fc9dcca6e2
Ignacy Zurawski <ignacyzurawski6@gmail.com>
2024-12-13 11:25:13 +0000

    skibidi

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,11 @@
+use candid::Nat;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk::query]
+fn calculate_currency_price(curr_a: Nat, curr_b: Nat,) -> Nat {
+    curr_a * curr_b
 }
--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,16 +1,29 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+import { conditionalDelay } from '@dfinity/agent/lib/cjs/polling/strategy';
+let rates = ref('');
+const iloscWaluty = ref(0)
 
 const getDataFromNBP = async () => {
   const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
   const jsonData = await res.json();
   console.log(jsonData)
-  const rates = jsonData[0].rates
+  rates.value = jsonData[0].rates
   console.log(rates)
+}
+
+const kupWalute = async (index) => {
+  console.log(rates.value[index])
+  const ilosc = BigInt(iloscWaluty.value)
+  const cena = BigInt(rates.value[index].mid * 10e16)
+  const koszt = await my_project_backend.calculate_currency_price(ilosc, cena)
+  console.log(koszt/BigInt(10e16))
+
+  my_project_backend.calculate_currency_price()
 
 }
+
 getDataFromNBP()
 </script>
 
@@ -19,7 +32,21 @@ getDataFromNBP()
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <input v-model="input" type="text" />
-    {{ input }}
+    <table>
+    <tr>
+      <th>Nazwa waluty</th>
+      <th>Kod waluty</th>
+      <th>Cena</th>
+      <th>Ilość waluty do zakupu</th>
+      <th></th>
+    </tr>
+    <tr v-for="(rate, index) in rates">
+      <td>{{ rate.currency }}</td>
+      <td>{{ rate.code }}</td>
+      <td>{{ rate.mid }}</td>
+      <td><input type="number" v-model="iloscWaluty" /></td>
+      <td><button @click="kupWalute(index)">KUP</button></td>
+    </tr>
+  </table>
   </main>
 </template>
