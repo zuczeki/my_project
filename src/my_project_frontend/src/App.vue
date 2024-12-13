78f905b5b8bffff4c91aa551b5d856d16d1efe4b
Ignacy Zurawski <ignacyzurawski6@gmail.com>
2024-12-13 09:38:50 +0000

    skibidi

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -3,14 +3,15 @@ import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
 let greeting = ref('');
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+const getDataFromNBP = async () => {
+  const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
+  const jsonData = await res.json();
+  console.log(jsonData)
+  const rates = jsonData[0].rates
+  console.log(rates)
+
 }
+getDataFromNBP()
 </script>
 
 <template>
@@ -18,11 +19,7 @@ async function handleSubmit(e) {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+    <input v-model="input" type="text" />
+    {{ input }}
   </main>
 </template>
